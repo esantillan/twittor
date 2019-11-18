ed9274a459117a676274188d22481351f68277b4
Esteban <estebansantillan@andesmar.com.ar>
2019-11-18 02:22:49 -0300

    arreglos para IOS (notch) y cache

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
 // import
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v1';
-const DYNAMIC_CACHE = 'dynamic-v1';
-const INMUTABLE_CACHE = 'inmutable-v1';
+const STATIC_CACHE = 'static-v2';
+const DYNAMIC_CACHE = 'dynamic-v2';
+const INMUTABLE_CACHE = 'inmutable-v2';
 
 const APP_SHELL = [
     // '/',
@@ -38,6 +38,12 @@ self.addEventListener('activate', e => {
     const respuesta = caches.keys().then(keys => {
         keys.forEach(key => {
             if (key !== STATIC_CACHE && key.includes('static')) {
+                return caches.delete(key);
+            }
+            if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
+                return caches.delete(key);
+            }
+            if (key !== INMUTABLE_CACHE && key.includes('inmutable')) {
                 return caches.delete(key);
             }
         });
